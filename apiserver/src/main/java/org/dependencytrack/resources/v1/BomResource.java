6363e58141f5ae09705d8fccee823c43177f5f1b
Alexis Lamb <alexis.lamb@lmco.com>
2025-05-27 10:42:13 -0400

    feat: uploadBomGitLab validates a GitLab ID Token with a public JWKS and then uploads a bom file
    Signed-off-by: Alexis Lamb <alexis.lamb@lmco.com>

--- apiserver/src/main/java/org/dependencytrack/resources/v1/BomResource.java
+++ apiserver/src/main/java/org/dependencytrack/resources/v1/BomResource.java
@@ -99,6 +99,7 @@ import net.minidev.json.JSONObject;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
 import java.math.BigInteger;
 import java.net.URI;
