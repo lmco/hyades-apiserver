fb5c2870ecce36a9f363719aaf39f9320b298f66
e403319 <carter.mitchell@lmco.com>
2026-05-13 16:15:05 +0000

    Remove gitlab bom upload for now

--- apiserver/src/main/java/org/dependencytrack/resources/v1/BomResource.java
+++ apiserver/src/main/java/org/dependencytrack/resources/v1/BomResource.java
@@ -22,12 +22,6 @@ import alpine.model.ConfigProperty;
 import alpine.server.auth.PermissionRequired;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.uuid.Generators;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
-import io.jsonwebtoken.security.SignatureException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -477,117 +471,6 @@ public class BomResource extends AbstractApiResource {
         }
 
         return processUpload(projectInfo, bomBytes);
-    }
-
-    @POST
-    @Path("/gitlab")
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Produces(MediaType.APPLICATION_JSON)
-    @Operation(summary = "Upload a supported bill of material from GitLab", description = "This endpoint processes input and delegates the request to the uploadBom method.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Token to be used for checking BOM processing progress", content = @Content(schema = @Schema(implementation = BomUploadResponse.class))),
-            @ApiResponse(responseCode = "400", description = "Invalid input"),
-            @ApiResponse(responseCode = "401", description = "Unauthorized"),
-            @ApiResponse(responseCode = "404", description = "The project could not be found")
-    })
-    @PermissionRequired(Permissions.Constants.BOM_UPLOAD)
-    public Response uploadBomGitLab(
-            @FormDataParam("gitLabToken") String idToken,
-            @FormDataParam("bom") String bom,
-            @FormDataParam("isLatest") @DefaultValue("false") boolean isLatest) {
-
-        try (QueryManager qm = new QueryManager()) {
-            Function<ConfigPropertyConstants, ConfigProperty> propertyGetter = cpc -> qm.getConfigProperty(
-                    cpc.getGroupName(),
-                    cpc.getPropertyName());
-
-            if (qm.isEnabled(GITLAB_ENABLED))
-                return Response.notModified("GitLab integration not enabled").build();
-
-            if (qm.isEnabled(GITLAB_SBOM_PUSH_ENABLED))
-                return Response.notModified("GitLab SBOM push functionality not enabled").build();
-
-            Boolean autoCreateProject = Boolean
-                    .parseBoolean(propertyGetter.apply(GITLAB_AUTOCREATE_PROJECTS).getPropertyValue());
-
-            if (idToken == null || !idToken.matches("^[\\w-]+\\.[\\w-]+\\.[\\w-]+$"))
-                return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid or missing GitLab idToken")
-                        .build();
-
-            ConfigProperty gitLabUrlProperty = propertyGetter.apply(GITLAB_URL);
-            String alpineIssuerProperty = ConfigProvider.getConfig().getConfigValue(ConfigKeys.OIDC_ISSUER).getValue();
-            String gitlabUrl = StringUtils.defaultIfBlank(alpineIssuerProperty, gitLabUrlProperty.getPropertyValue());
-            ConfigProperty gitLabJwksPathProperty = propertyGetter.apply(GITLAB_JWKS_PATH);
-
-            // Get the key id (kid) from the JWT header
-            String headerJson = new String(Base64.getUrlDecoder().decode(idToken.split("\\.")[0]));
-            String kid = (String) new ObjectMapper().readValue(headerJson, Map.class).get("kid");
-
-            Claims claims = Jwts.parser()
-                    .verifyWith(GitLabClient.getPublicKeyFromJwks(gitlabUrl, gitLabJwksPathProperty.getPropertyValue(), kid))
-                    .build()
-                    .parseSignedClaims(idToken)
-                    .getPayload();
-
-            // If autoCreate is enabled and the project doesn't exist, create the project
-            final String projectName = List.of(claims.get(GitLabClient.PROJECT_PATH_CLAIM, String.class).split("/"))
-                    .getLast();
-            final String projectVersion = claims
-                    .get(claims.get(GitLabClient.REF_TYPE_CLAIM, String.class).equals("tag") ? "ref"
-                            : GitLabClient.REF_PATH_CLAIM, String.class);
-            Project project = qm.getProject(projectName, projectVersion);
-
-            String accessLevel = claims.get(GitLabClient.USER_ACCESS_LEVEL_CLAIM, String.class);
-            if (accessLevel == null) {
-                return Response.status(Response.Status.BAD_REQUEST)
-                        .entity("Missing user_access_level claim in token").build();
-            }
-
-            if (project == null) {
-                if (autoCreateProject
-                        && Set.of("owner", "maintainer")
-                                .contains(claims.get(GitLabClient.USER_ACCESS_LEVEL_CLAIM, String.class)))
-                    createNewProject(projectName, projectVersion, null, null, isLatest);
-                else
-                    return Response.status(Response.Status.UNAUTHORIZED)
-                            .entity("The principal does not have permission to create project.").build();
-            }
-
-            if (claims.get(GitLabClient.PROJECT_PATH_CLAIM, String.class) == null)
-                return Response.status(Response.Status.BAD_REQUEST).entity("Missing project_path claim").build();
-
-            if (!claims.get(GitLabClient.REF_TYPE_CLAIM, String.class).equals("tag")
-                    && claims.get(GitLabClient.REF_PATH_CLAIM, String.class) == null)
-                return Response.status(Response.Status.BAD_REQUEST).entity("Invalid ref_type or missing ref_path claim")
-                        .build();
-
-            BomSubmitRequest bomSubmitRequest = new BomSubmitRequest(
-                    null,
-                    projectName,
-                    projectVersion,
-                    null,
-                    autoCreateProject,
-                    isLatest,
-                    bom);
-
-            return uploadBom(bomSubmitRequest);
-        } catch (SignatureException e) {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity("Received token that did not pass signature verification").build();
-        } catch (ExpiredJwtException e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity("Received expired token").build();
-        } catch (MalformedJwtException e) {
-            return Response.status(Response.Status.BAD_REQUEST).entity("Received malformed token").build();
-        } catch (UnsupportedJwtException | IllegalArgumentException e) {
-            LOGGER.error(SecurityMarkers.SECURITY_FAILURE, e.getMessage());
-            return Response.status(Response.Status.BAD_REQUEST).entity("Received unsupported JWT").build();
-        } catch (IOException e) {
-            LOGGER.error(SecurityMarkers.EVENT_FAILURE, "Error reading or parsing the JWT header or JWKS: " + e.getMessage());
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-        } catch (Exception e) {
-            LOGGER.error(SecurityMarkers.EVENT_FAILURE, "An error occured in uploadBomGitLab: " + e.getMessage());
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-        }
     }
 
     @POST
